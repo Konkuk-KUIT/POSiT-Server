5fbd4a117cf8ac1b9fa4f9d85d7f8994963d8096
bucheasal <leejunwoo9@gmail.com>
2026-01-02 16:23:27 +0900

    init: spring boot project

---
+++ src/main/java/com/posit/posit/PositApplication.java
@@ -0,0 +1,13 @@
+package com.posit.posit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/posit/posit/PositApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posit.posit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
