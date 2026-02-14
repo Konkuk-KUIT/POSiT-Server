c965aae2238d92c2d0414da678c85da3d87ac890
bucheasal <leejunwoo9@gmail.com>
2026-02-14 12:47:52 +0900

    사장님 고민 등록 URL 수정 /stores/concerns -> /owner/concerns

--- src/main/java/com/posit/posit/domain/store/controller/OwnerController.java
+++ src/main/java/com/posit/posit/domain/store/controller/OwnerController.java
@@ -50,7 +50,7 @@ public class OwnerController {
 
     // 3. 고민 등록
     @Operation(summary = "고민 등록", description = "가게에 대한 고민글을 작성합니다.")
-    @PostMapping("/stores/concerns")
+    @PostMapping("/owner/concerns")
     public ResponseEntity<ApiResponse<ConcernCreateResponse>> createConcern(
             @AuthenticationPrincipal UserPrincipal user,
             @RequestBody @Valid ConcernCreateRequest request
